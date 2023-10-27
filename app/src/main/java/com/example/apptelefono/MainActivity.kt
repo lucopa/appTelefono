dac6ea24f8f0fba9cab956e3b1208ae01796c716
lucopa <lcobpar754@g.educaand.es>
2023-10-27 21:19:42 +0200

    Segundo activity creado y boton llamada

--- app/src/main/java/com/example/apptelefono/MainActivity.kt
+++ app/src/main/java/com/example/apptelefono/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.apptelefono
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val boton : Button = findViewById(R.id.llamada)
+        boton.setOnClickListener{
+            val intent: Intent = Intent (this, Pantalla2:: class.java)
+            startActivity(intent)
+        }
     }
 }
