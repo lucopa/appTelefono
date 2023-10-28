19baaaee196761fb4d611f891e9c1ab20aec148e
lucopa <lcobpar754@g.educaand.es>
2023-10-28 16:22:05 +0200

    Boton llamada, url realizado y boton alarma error

--- app/src/main/java/com/example/apptelefono/MainActivity.kt
+++ app/src/main/java/com/example/apptelefono/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.apptelefono
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,5 +19,32 @@ class MainActivity : AppCompatActivity() {
             val intent: Intent = Intent (this, Pantalla2:: class.java)
             startActivity(intent)
         }
+
+
+        val alarma: Button = findViewById(R.id.botonAlarma)
+        alarma.setOnClickListener() {
+            abrirAppAlarmaConAlarmaPredeterminada()
+        }
+
+        val botonURL: Button = findViewById(R.id.url)
+            botonURL.setOnClickListener {
+            abrirURL("https://www.wikipedia.com")
+        }
     }
-}
+
+    private fun abrirURL(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(intent)
+    }
+
+    }
+    private fun abrirAppAlarmaConAlarmaPredeterminada() {
+        val intent = Intent(AlarmClock.ACTION_SET_ALARM).apply {
+            putExtra(AlarmClock.EXTRA_HOUR, 8) // Hora de la alarma (formato de 24 horas)
+            putExtra(AlarmClock.EXTRA_MINUTES, 0) // Minutos de la alarma
+            putExtra(AlarmClock.EXTRA_MESSAGE, "Alarma predeterminada") // Mensaje de la alarma
+            putExtra(AlarmClock.EXTRA_SKIP_UI, true) // Opcional: Salta la interfaz de usuario de configuración de la alarma
+
+        }
+
+    }
--- app/src/main/java/com/example/apptelefono/Pantalla2.kt
+++ app/src/main/java/com/example/apptelefono/Pantalla2.kt
@@ -1,11 +1,52 @@
 package com.example.apptelefono
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 
 class Pantalla2 : AppCompatActivity() {
+    private val REQUEST_CALL_PERMISSION = 1
+
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pantalla2)
+
+        val botonLlamada: ImageButton = findViewById(R.id.boton_llamada)
+        botonLlamada.setOnClickListener {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.CALL_PHONE
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                hacerLlamada()
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.CALL_PHONE),
+                    REQUEST_CALL_PERMISSION
+                )
+            }
+        }
     }
+
+
+    private fun hacerLlamada() {
+        val numeroTelefono = "tel:674695275"
+        val intentLlamada = Intent(Intent.ACTION_CALL, Uri.parse(numeroTelefono))
+        startActivity(intentLlamada)
+    }
+
+
+
+
+
 }
+
